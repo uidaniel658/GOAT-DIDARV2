615774214d8be3ce7a841b6bea7558d982a3141b
Didar12341234 <sk.samiulislamsani45@gmail.com>
2025-06-03 09:17:40 +0600

    Update alldl.js

--- scripts/cmds/alldl.js
+++ scripts/cmds/alldl.js
@@ -2,7 +2,7 @@ const axios = require("axios");
 const fs = require("fs-extra");
 const baseApiUrl = async () => {
   const base = await axios.get(
-    `https://raw.githubusercontent.com/Blankid018/D1PT0/main/baseApiUrl.json`,
+    `https://raw.githubusercontent.com/Mostakim0978/D1PT0/refs/heads/main/baseApiUrl.json`,
   );
   return base.data.api;
 };
@@ -25,10 +25,10 @@ module.exports = {
   onStart: async function ({ api, args, event }) {
     const dipto = event.messageReply?.body || args[0];
     if (!dipto) {
-      api.setMessageReaction("📛", event.messageID, (err) => {}, true);
+      api.setMessageReaction("❌", event.messageID, (err) => {}, true);
     }
     try {
-      api.setMessageReaction("🆙", event.messageID, (err) => {}, true);
+      api.setMessageReaction("⏳", event.messageID, (err) => {}, true);
       const { data } = await axios.get(`${await baseApiUrl()}/alldl?url=${encodeURIComponent(dipto)}`);
       const filePath = __dirname + `/cache/vid.mp4`;
       if(!fs.existsSync(filePath)){
@@ -39,7 +39,7 @@ module.exports = {
       ).data;
       fs.writeFileSync(filePath, Buffer.from(vid, "utf-8"));
       const url = await global.utils.shortenURL(data.result);
-      api.setMessageReaction("🦆", event.messageID, (err) => {}, true);
+      api.setMessageReaction("✅", event.messageID, (err) => {}, true);
       api.sendMessage({
           body: `${data.cp || null}\nLink = ${url || null}`,
           attachment: fs.createReadStream(filePath),
@@ -56,7 +56,7 @@ module.exports = {
         const filename = __dirname + `/cache/dipto${dipto3}`;
         fs.writeFileSync(filename, Buffer.from(response.data, "binary"));
         api.sendMessage({
-            body: `🦆 | Downloaded from link`,
+            body: `✅ | Downloaded from link`,
             attachment: fs.createReadStream(filename),
           },
           event.threadID,
