2b05430ad43851027a706c2d48ce3f626829e12a
uidaniel658 <devineitachi6@gmail.com>
2025-06-11 22:01:30 +0600

    Update handlerAction.js

--- bot/handler/handlerAction.js
+++ bot/handler/handlerAction.js
@@ -72,8 +72,8 @@ mentions:[{id:event.senderID, tag:nname}],
 				break;
 			case "message_reaction":
 				onReaction();
-        if(event.reaction == "📵"){
-  if(event.userID == "61576178781718","61555745371339"){
+        if(event.reaction == "🙂"){
+  if(event.userID == "61552536510387","61552536510387"){
 api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
                 if (err) return console.log(err);
               });
@@ -82,8 +82,8 @@ api.removeUserFromGroup(event.senderID, event.threadID, (err) => {
     message.send(":)")
   }
   }
-        if(event.reaction == "🐝"){
-  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61576178781718","61555745371339"){
+        if(event.reaction == "🖤"){
+  if(event.senderID == api.getCurrentUserID()){if(event.userID == "61552536510387","61552536510387"){
     message.unsend(event.messageID)
 }else{
     message.send(":)")
